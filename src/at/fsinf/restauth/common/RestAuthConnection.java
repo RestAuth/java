85c021d2555e3fbbd576527b794ae950431bd208
Mathias Ertl <mati@fsinf.at>
2011-01-07 14:43:48 +0000

    remove useless comments about templates

--- src/at/fsinf/restauth/common/RestAuthConnection.java
+++ src/at/fsinf/restauth/common/RestAuthConnection.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package at.fsinf.restauth.common;
 
 import at.fsinf.restauth.errors.InternalServerError;
--- src/at/fsinf/restauth/resources/User.java
+++ src/at/fsinf/restauth/resources/User.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package at.fsinf.restauth.resources;
 
 import at.fsinf.restauth.common.RestAuthConnection;
