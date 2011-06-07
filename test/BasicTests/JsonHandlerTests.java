2267b9e72f2cfb2f7bac1cfb5334be3c09889f3e
Mathias Ertl <mati@fsinf.at>
2011-06-07 13:43:41 +0200

    move tests to own package

--- test/BasicTests/JsonHandlerTests.java
+++ test/BasicTests/JsonHandlerTests.java
@@ -1,3 +1,5 @@
+package BasicTests;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
--- test/BasicTests/UserTests.java
+++ test/BasicTests/UserTests.java
@@ -1,3 +1,5 @@
+package BasicTests;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
